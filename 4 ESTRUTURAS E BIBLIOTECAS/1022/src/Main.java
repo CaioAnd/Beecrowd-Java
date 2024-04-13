b2ea8c860c0204d4bed10e1e41d2ba6d6234b579
CaioAndrade <caio.mk.andrade@gmail.com>
2024-04-13 01:15:27 -0300

    WIP 1022

---
+++ 4 ESTRUTURAS E BIBLIOTECAS/1022/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int quantidade = scanner.nextInt();
+        
+        for (int i = 0; i <= quantidade; i++) {
+            String expressao = scanner.nextLine();
+        }
+    }
+ 
+}
