7c6606b6148f6d043aa85eb709ef140eacfaf7a1
Caio Andrade <caio.mk.andrade@gmail.com>
2024-04-19 19:49:53 -0300

    [WIP] 1061

---
+++ 1 INICIANTE/1061/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String diaInicio = sc.nextLine();
+        String horarioInicio = sc.nextLine();
+        String diaFim = sc.nextLine();
+        String horarioFim = sc.nextLine();
+
+        
+
+        sc.close();
+    }
+ 
+}
